0a6861edbf5a352f988bf257345253a9b850c831
Juliano Oliveira <julianoorleans03@gmail.com>
2020-11-27 08:51:23 -0300

    ...

--- src/repositories/reserva-repository.js
+++ src/repositories/reserva-repository.js
@@ -3,35 +3,35 @@ const mongoose = require("mongoose");
 const Reserva = mongoose.model("Reserva");
 
 exports.create = async (data) => {
-    var reserva = new Reserva(data);
-    await reserva.save();
+  var reserva = new Reserva(data);
+  await reserva.save();
 };
 
 exports.search = async () => {
-    const res = await Reserva.find();
-    return res;
+  const res = await Reserva.find();
+  return res;
 };
 
 exports.confirmarReserva = async (id, data) => {
-    await Reserva.findByIdAndUpdate(id, {
-        $set: {
-            status: "confirmada"
-        },
-    });
+  await Reserva.findByIdAndUpdate(id, {
+    $set: {
+      status: "confirmada"
+    },
+  });
 };
 
 exports.cancelarReserva = async (id, data) => {
-    await Reserva.findByIdAndUpdate(id, {
-        $set: {
-            status: "cancelada"
-        },
-    });
+  await Reserva.findByIdAndUpdate(id, {
+    $set: {
+      status: "cancelada"
+    },
+  });
 };
 
 exports.put = async (id, data) => {
-    await Reserva.findByIdAndUpdate(id, {
-        $set: {
-            titulo: data
-        },
-    });
+  await Reserva.findByIdAndUpdate(id, {
+    $set: {
+      titulo: data
+    },
+  });
 };
